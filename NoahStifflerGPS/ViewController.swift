52ec4abac1c6b540fb4391373b4ec4799ab4ab46
Noah Stiffler <stifflernoah@gmail.com>
2020-04-01 13:52:47 -0400

    Initial Commit

---
+++ NoahStifflerGPS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoahStifflerGPS
+//
+//  Created by Noah Stiffler on 4/1/20.
+//  Copyright © 2020 Noah Stiffler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
